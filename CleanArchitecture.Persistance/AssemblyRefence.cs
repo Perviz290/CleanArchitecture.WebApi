dd6a9b32b29a5aab79f6b6326020aaeb5c682938
Perviz290 <veliyevperviz290@gmail.com>
2024-01-20 16:33:41 +0400

    sql-server connection and configuration file for entites

---
+++ CleanArchitecture.Persistance/AssemblyRefence.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace CleanArchitecture.Persistance;
+
+public static class AssemblyRefence
+{
+    public static readonly Assembly Assembly=typeof(Assembly).Assembly;
+
+    //Configuration folderin butun classlari gormek ucun AssemblyRefence Classi=>
+    // AppDbContext e tanitmaq lazimdir
+
+    //ForExample => protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+    //modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyRefence).Assembly);
+    //bunu appDbContext e yazmaq lazimdir
+}
---
+++ CleanArchitecture.Presentation/AssemblyReference.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace CleanArchitecture.Presentation
+{
+    public static class AssemblyReference
+    {
+
+        public static readonly Assembly Assembly=typeof(Assembly).Assembly;
+
+        //Bu assembly reference Web Api projectin program.cs file na bildirmek lazmdirki->
+        //Presentation icindeki Controlleri Web Api projectimiz gorsun 
+
+        //builder.Services.AddControllers().AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReference).Assembly);
+        // bunu program.cs e yazmaq lazimdir ki Burda olan butun Controlleri gorsun\
+
+    }
+}
